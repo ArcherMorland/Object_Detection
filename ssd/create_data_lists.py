b8229c65c148051862221bbd66a2edb06d612b34
Archer <38164406+ArcherMorland@users.noreply.github.com>
2020-07-05 17:30:06 +0800

    Delete create_data_lists.py

--- ssd/create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils import create_data_lists
-
-if __name__ == '__main__':
-    create_data_lists(train_path='../../dataset/voc2012/train.txt',
-                      valid_path='../../dataset/voc2012/valid.txt',
-                      output_folder='./')
